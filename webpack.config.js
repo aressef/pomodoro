64fbf4fe30e3f8bcf9c388ee77add0787c70b748
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-21 19:42:54 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const nodeEnv = process.env.NODE_ENV || 'production';
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    filename: './js/app.js'
+  },
+  output: {
+    filename: './js/_build/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ["es2015", { "modules": false }],
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    // uglify js
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false },
+      sourceMap: true,
+    }),
+    // env plugin
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+    }),
+  ],
+};

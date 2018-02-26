22d61ab9481b98bd5e626a638e5e7465ec86e985
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-26 17:00:52 -0500

    Tweak timer function

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+const displayTimer = document.querySelector('.timer-display');
+const startButton = document.querySelector('.start-button');
+// const stopButton = document.querySelector('.stop-button');
+
 function timer(duration) {
   const initialTime = Date.now();
-  const diff;
-  const minutes;
-  const seconds;
+  let diff;
+  let minutes;
+  let seconds;
 
   function recordTime() {
     // the difference between inital start and time recordTime is called
-    diff = duration  - (((Date.now() - initalStart) / 1000) | 0);
+    diff = duration - (((Date.now() - initialTime) / 1000) | 0);
 
     // create minutes and seconds
     minutes = (diff / 60) | 0;
@@ -14,22 +18,19 @@ function timer(duration) {
 
     // if minutes or seconds are less than 10 add a zero to the front
     // example: 9 seconds becomes 09 seconds
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
     // add time to texContent of displayTimer
     displayTimer.textContent = `${minutes}:${seconds}`;
   }
-  timer();
-  setInterval(timer, 1000);
+  recordTime();
+  setInterval(recordTime, 1000);
 }
 
 function startTimer(time) {
-  var timerAmount = time * 60;
-  timer(time);
+  const timerAmount = time * 60;
+  timer(timerAmount);
 }
 
-
-const displayTimer = document.querySelector('.timer-display');
-const startButton = document.querySelector('.start-button');
-const stopButton = document.querySelector('.stop-button');
+startButton.addEventListener('click', () => startTimer(5));

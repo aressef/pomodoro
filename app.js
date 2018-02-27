5d8d1cebe58d12dead42c1be6a40f16d5c527fb6
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-27 11:14:56 -0500

    Refactor recordTime function

--- app.js
+++ app.js
@@ -10,20 +10,26 @@ function timer(duration) {
 
   function recordTime() {
     // the difference between inital start and time recordTime is called
-    diff = duration - (((Date.now() - initialTime) / 1000) | 0);
+    diff = duration - parseInt(((Date.now() - initialTime) / 1000), 10);
 
     // create minutes and seconds
-    minutes = (diff / 60) | 0;
-    seconds = (diff % 60) | 0;
+    minutes = parseInt(diff / 60, 10);
+    seconds = parseInt(diff % 60, 10);
 
     // if minutes or seconds are less than 10 add a zero to the front
     // example: 9 seconds becomes 09 seconds
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    // add time to texContent of displayTimer
+    // add time to textContent of displayTimer
     displayTimer.textContent = `${minutes}:${seconds}`;
+
+    // check if timer has run out
+    // if (displayTimer.textContent === '00:00') {
+    //   return;
+    // }
   }
+
   recordTime();
   setInterval(recordTime, 1000);
 }
@@ -33,4 +39,4 @@ function startTimer(time) {
   timer(timerAmount);
 }
 
-startButton.addEventListener('click', () => startTimer(5));
+startButton.addEventListener('click', () => startTimer(.1));

30aa97bed5c9304f355e55f2c74c0350264f91a2
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-02-27 16:36:22 -0500

    Fix timer to stop at 00:00

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const startButton = document.querySelector('.start-button');
 
 function timer(duration) {
   const initialTime = Date.now();
-  let diff;
-  let minutes;
-  let seconds;
+  let testToStop;
+  let continueCounting;
 
   function recordTime() {
+    if (testToStop === true) {
+      clearInterval(continueCounting);
+      return;
+    }
     // the difference between inital start and time recordTime is called
-    diff = duration - parseInt(((Date.now() - initialTime) / 1000), 10);
+    const diff = duration - parseInt(((Date.now() - initialTime) / 1000), 10);
 
     // create minutes and seconds
-    minutes = parseInt(diff / 60, 10);
-    seconds = parseInt(diff % 60, 10);
+    let minutes = parseInt(diff / 60, 10);
+    let seconds = parseInt(diff % 60, 10);
 
     // if minutes or seconds are less than 10 add a zero to the front
     // example: 9 seconds becomes 09 seconds
@@ -25,13 +28,13 @@ function timer(duration) {
     displayTimer.textContent = `${minutes}:${seconds}`;
 
     // check if timer has run out
-    // if (displayTimer.textContent === '00:00') {
-    //   return;
-    // }
+    if (displayTimer.textContent === '00:00') {
+      testToStop = true;
+    }
   }
 
   recordTime();
-  setInterval(recordTime, 1000);
+  continueCounting = setInterval(recordTime, 1000);
 }
 
 function startTimer(time) {
